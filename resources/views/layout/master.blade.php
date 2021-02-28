68a7ce184cd42cf11eb08d5e1eee371972f6c457
hoaithuong2002 <dgth2o2@gmail.com>
2021-02-28 22:54:11 +0700

    template

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard 2</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{asset('css/OverlayScrollbars.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+    <!-- Navbar -->
+    @include('core.navbar')
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+@include('core.menu-left')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+     @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    @include('core.footer')
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('js/adminlte.js')}}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+{{--<script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>--}}
+{{--<script src="plugins/raphael/raphael.min.js"></script>--}}
+{{--<script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>--}}
+{{--<script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>--}}
+{{--<!-- ChartJS -->--}}
+{{--<script src="plugins/chart.js/Chart.min.js"></script>--}}
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('js/dashboard2.js')}}"></script>
+</body>
+</html>
