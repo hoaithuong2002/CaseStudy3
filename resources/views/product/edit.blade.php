801e00a1a44957b6bc9101ce84a72f73ddf2c5d9
hoaithuong2002 <dgth2o2@gmail.com>
2021-04-07 13:36:48 +0700

    toastr, crud product

---
+++ resources/views/product/edit.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.master')
+@section('content')
+    <div class="card mt-2">
+        <div class="card-header">
+            Edit Product
+        </div>
+        <div class="card-body">
+            <form method="post" action="{{ route('product.update', $product->id) }}">
+                @csrf
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="name" value="{{ $product->name }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Description</label>
+                    <input type="text" value="{{ $product->description }}" name="description" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Content</label>
+                    <input type="text" value="{{ $product->content }}" name="content" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Image</label>
+                    <input type="text" value="{{ $product->image }}" name="image" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Price</label>
+                    <input type="number" value="{{ $product->price }}" name="price" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+
+{{--@section('content')--}}
+
+{{--    <section class="content">--}}
+
+{{--        @csrf--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-md-12 col-md-6">--}}
+{{--                <div class="card card-primary">--}}
+{{--                    <div class="card-header">--}}
+{{--                        <h3 class="card-title">Thêm Sản Phẩm</h3>--}}
+{{--                        <div class="card-tools">--}}
+{{--                            <button type="button" class="btn btn-tool" data-card-widget="collapse"--}}
+{{--                                    data-toggle="tooltip" title="Collapse">--}}
+{{--                                <i class="fas fa-minus"></i></button>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <form method="post" action="{{route('product.update',$product->id)}}" enctype="multipart/form-data">--}}
+{{--                        @csrf--}}
+{{--                        <div class="card-body">--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="inputName">Name</label>--}}
+{{--                                <input type="text" name="name"--}}
+{{--                                       class="form-control" value="{{$product->name}}">--}}
+{{--                                --}}{{--                                @error('name')--}}
+{{--                                --}}{{--                                <p class="wrapper">{{$message}}</p>--}}
+{{--                                --}}{{--                                @enderror--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="inputName">Description</label>--}}
+{{--                                <input type="text" name="description"--}}
+{{--                                       class="form-control" value="{{$product->description}}">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="inputName">Content</label>--}}
+{{--                                <input type="text" name="content"--}}
+{{--                                       class="form-control" value="{{$product->content}}">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="exampleInputFile">Image</label>--}}
+{{--                                <div class="input-group">--}}
+{{--                                    <div class="custom-file">--}}
+{{--                                        <input type="file" name="image" class="custom-file-input" value="{{$product->image}}">--}}
+{{--                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="input-group-append">--}}
+{{--                                        <span class="input-group-text">Upload</span>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="inputName">Price</label>--}}
+{{--                                <input type="number" name="price"--}}
+{{--                                       class="form-control" value="{{$product->price}}">--}}
+{{--                            </div>--}}
+
+{{--                            <div class="form-group">--}}
+{{--                                <button type="submit" class="btn btn-success">Save</button>--}}
+{{--                                <a href="{{route('product.index')}} " class="btn btn-secondary">Back</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+
+{{--                    <!-- /.card-body -->--}}
+{{--                </div>--}}
+{{--                <!-- /.card -->--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </section>--}}
+{{--@endsection--}}
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"></h3>
+                    <a href="{{route('product.create')}}" type="submit" class="btn btn-success"> Add</a>
+                    <div class="card-tools">
+                        <form action="{{route('product.search')}}" method="post">
+                            @csrf
+                            <div class="input-group input-group-sm" style="width: 150px;">
+                                <input type="text" name="keyword" class="form-control float-right"
+                                       placeholder="Search">
+
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover text-nowrap">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Content</th>
+                            <th>Image</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach($products as $product)
+                            <tr>
+                                <td>{{$product->id}}</td>
+                                <td>{{$product->name}}</td>
+                                <td>{{$product->description}}</td>
+                                <td>{{$product->content}}</td>
+                                <td>{{$product->image}}</td>
+                                <td>{{$product->price}}</td>
+                                <td>
+                                    <a href="{{route('product.edit', $product -> id)}}" class="btn btn-primary">Edit</a>
+                                    <a onclick="return confirm('Bạn có chắc chắn muốn xóa : {{$product->name}} không???')"
+                                       href="{{route('product.destroy', $product->id)}}"
+                                       class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Content</th>
+                            <th>Image</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                    <div class="d-flex justify-content-center">
+                        {{ $products->links() }}
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+@endsection
